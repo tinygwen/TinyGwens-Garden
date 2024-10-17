47e6fbc8a82b6f0243ccf3ad8265e8d2a0ba6430
Gwen <tinygwen@proton.me>
2024-10-17 06:28:18 -0500

    added quartz and info about how i wrote the site with obsidian

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
        provider: "plausible",
     },*/
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz", /** CHANGE ME */
+    baseUrl: "https://tinygwen.github.io/TinyGwens-Garden/", /** CHANGE ME */
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
